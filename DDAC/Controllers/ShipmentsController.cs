ff5f2d5ce401c6c38706ceee2240731613636269
Karin <karinfam@outlook.com>
2016-07-26 21:55:51 +0800

    added basic pages

---
+++ DDAC/Controllers/ShipmentsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DDAC.Models;
+
+namespace DDAC.Controllers
+{
+    public class ShipmentsController : Controller
+    {
+        private DDACEntities db = new DDACEntities();
+
+        // GET: Shipments
+        public ActionResult Index()
+        {
+            var shipments = db.Shipments.Include(s => s.Customer).Include(s => s.Ship).Include(s => s.Yard);
+            return View(shipments.ToList());
+        }
+
+        // GET: Shipments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shipment shipment = db.Shipments.Find(id);
+            if (shipment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shipment);
+        }
+
+        // GET: Shipments/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name");
+            ViewBag.ShipID = new SelectList(db.Ships, "ShipID", "ShipID");
+            ViewBag.YardID = new SelectList(db.Yards, "YardID", "YardName");
+            return View();
+        }
+
+        // POST: Shipments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShipmentID,Type,InsuredValue,Weight,Destination,Status,ShipID,YardID,CustomerID")] Shipment shipment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shipments.Add(shipment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name", shipment.CustomerID);
+            ViewBag.ShipID = new SelectList(db.Ships, "ShipID", "ShipID", shipment.ShipID);
+            ViewBag.YardID = new SelectList(db.Yards, "YardID", "YardName", shipment.YardID);
+            return View(shipment);
+        }
+
+        // GET: Shipments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shipment shipment = db.Shipments.Find(id);
+            if (shipment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name", shipment.CustomerID);
+            ViewBag.ShipID = new SelectList(db.Ships, "ShipID", "ShipID", shipment.ShipID);
+            ViewBag.YardID = new SelectList(db.Yards, "YardID", "YardName", shipment.YardID);
+            return View(shipment);
+        }
+
+        // POST: Shipments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShipmentID,Type,InsuredValue,Weight,Destination,Status,ShipID,YardID,CustomerID")] Shipment shipment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shipment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name", shipment.CustomerID);
+            ViewBag.ShipID = new SelectList(db.Ships, "ShipID", "ShipID", shipment.ShipID);
+            ViewBag.YardID = new SelectList(db.Yards, "YardID", "YardName", shipment.YardID);
+            return View(shipment);
+        }
+
+        // GET: Shipments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shipment shipment = db.Shipments.Find(id);
+            if (shipment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shipment);
+        }
+
+        // POST: Shipments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Shipment shipment = db.Shipments.Find(id);
+            db.Shipments.Remove(shipment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DDAC/Controllers/YardsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DDAC.Models;
+
+namespace DDAC.Controllers
+{
+    public class YardsController : Controller
+    {
+        private DDACEntities db = new DDACEntities();
+
+        // GET: Yards
+        public ActionResult Index()
+        {
+            return View(db.Yards.ToList());
+        }
+
+        // GET: Yards/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yard yard = db.Yards.Find(id);
+            if (yard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yard);
+        }
+
+        // GET: Yards/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Yards/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "YardID,YardName,Port")] Yard yard)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Yards.Add(yard);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(yard);
+        }
+
+        // GET: Yards/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yard yard = db.Yards.Find(id);
+            if (yard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yard);
+        }
+
+        // POST: Yards/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "YardID,YardName,Port")] Yard yard)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(yard).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(yard);
+        }
+
+        // GET: Yards/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yard yard = db.Yards.Find(id);
+            if (yard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yard);
+        }
+
+        // POST: Yards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Yard yard = db.Yards.Find(id);
+            db.Yards.Remove(yard);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
